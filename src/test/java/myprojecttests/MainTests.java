afc5143de1ae0ed1bbdea2ef8f10ae9c9762c7c1
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-06-25 18:36:36 +0200

    configure two versions

---
+++ src/test/java/myprojecttests/MainTests.java
@@ -0,0 +1,17 @@
+package myprojecttests;
+
+//import org.junit.Test;
+
+//import myproject.Main;
+
+//import static org.junit.Assert.assertEquals;
+
+public class MainTests {
+	/*@Test
+    public void bankAccounVersionTest() {
+        Main bankSystem = new Main();
+        boolean isProVersion = true;
+		assertEquals(8, bankAccount.getId(), 0.01);
+    }*/
+
+}
