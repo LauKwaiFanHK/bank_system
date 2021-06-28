c23fc32db5466a0a89b807db8fddadf0e4b9bfc5
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-06-28 16:54:38 +0200

    javadoc for BankAccount class

--- src/main/java/myproject/BankAccount.java
+++ src/main/java/myproject/BankAccount.java
@@ -1,13 +1,23 @@
 package myproject;
 
+/**
+ * Represents the setting up for a standard bank account.
+ * 
+ */
 public class BankAccount extends BaseAccount {
 
+	/**
+	 * Return a message that credit service is only available for professional version.
+	 */
 	@Override
 	public double getCredit() {
 		System.out.println("This bank account type does not support this function. Please upgrade to professional bank account.");
 		return 0;
 	}
 
+	/**
+	 * Return a message that credit service is only available for professional version.
+	 */
 	@Override
 	public void setCredit(double d) {
 		System.out.println("This bank account type does not support this function. Please upgrade to professional bank account.");
