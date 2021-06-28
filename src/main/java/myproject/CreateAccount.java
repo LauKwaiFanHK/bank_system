e12a740b9fa2679b8a4170dd46681daa7c226244
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-06-28 17:22:55 +0200

    Javadoc for CreateAccount class

--- src/main/java/myproject/CreateAccount.java
+++ src/main/java/myproject/CreateAccount.java
@@ -2,11 +2,19 @@ package myproject;
 
 import java.util.List;
 
+/** Represents the creation of an standard bank account.
+ * @author LauKwaiFanHK
+ * @version 1.0
+ * @since 1.0
+*/
 public class CreateAccount {
 	
 	private static final String COMMAND = "create";
     private static final int NUMBER_OF_ARG = 0;
 
+    /** Executes the command to create a standard bank account.
+     *@return an integer representing the id of the created account. 
+     */
     public int executeCreateAccount(String input, int numberOfArg, List<BaseAccount> list) {
         if (verifyCommand(input, numberOfArg)) {
             BankAccount bankAccount = new BankAccount();
@@ -21,6 +29,9 @@ public class CreateAccount {
         }
     }
 
+    /** Verifies the input command.
+     *@return an boolean representing the state of verification. 
+     */
     private boolean verifyCommand(String input, int numberOfArg) {
         return COMMAND.equals(input) && NUMBER_OF_ARG == numberOfArg;
     }
