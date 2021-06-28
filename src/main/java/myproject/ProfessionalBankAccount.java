613fac452946777593c4e997855376b63cfee35d
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-06-28 17:43:46 +0200

    Javadoc for ProfessionalBankAccount class

--- src/main/java/myproject/ProfessionalBankAccount.java
+++ src/main/java/myproject/ProfessionalBankAccount.java
@@ -1,13 +1,24 @@
 package myproject;
 
+/** Represents an professional bank account.
+ * @author Group3
+ * @version 1.5
+ * @since 1.0
+*/
 public class ProfessionalBankAccount extends BaseAccount {
 
     private double credit;
    
+    /** Gets the granted credit to a professional bank account owner.
+     * @return A double representing the granted credit amount.   
+     */
     public double getCredit() {
         return credit;
     }
 
+    /** Sets a granted credit for a professional bank account
+     * @param credit A double containing the amount of the granted credit.
+     */
     public void setCredit(double credit) {
         this.credit = credit;
     }
