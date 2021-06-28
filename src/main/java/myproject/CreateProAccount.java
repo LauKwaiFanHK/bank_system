d55c61e616368c7a8d2350a1b11a7a3a60b9ebf3
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-06-28 17:25:37 +0200

    Javadoc for CreateProAccount class

--- src/main/java/myproject/CreateProAccount.java
+++ src/main/java/myproject/CreateProAccount.java
@@ -2,10 +2,18 @@ package myproject;
 
 import java.util.List;
 
+/** Represents the creation of an professional account.
+ * @author LauKwaiFanHK
+ * @version 1.5
+ * @since 1.0
+*/
 public class CreateProAccount {
 	private static final String COMMAND = "create";
     private static final int NUMBER_OF_ARG = 0;
 
+    /** Executes the command to create a professional bank account.
+     *@return an integer representing the id of the created professional bank account. 
+     */
     public int executeCreateProAccount(String input, int numberOfArg, List<BaseAccount> list) {
         if (verifyCommand(input, numberOfArg)) {
             ProfessionalBankAccount proBankAccount = new ProfessionalBankAccount();
@@ -21,6 +29,9 @@ public class CreateProAccount {
         }
     }
 
+    /** Verifies the input command.
+     *@return an boolean representing the state of verification. 
+     */
     private boolean verifyCommand(String input, int numberOfArg) {
         return COMMAND.equals(input) && NUMBER_OF_ARG == numberOfArg;
     }
