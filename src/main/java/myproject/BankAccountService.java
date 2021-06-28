caf2f750b9a53a56687a3ea25f7d26d3b66bf059
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2021-06-28 17:18:49 +0200

    Javadoc for BankAccountService class

--- src/main/java/myproject/BankAccountService.java
+++ src/main/java/myproject/BankAccountService.java
@@ -2,22 +2,32 @@ package myproject;
 
 import java.util.List;
 /**
- * BankAccountService contains methods that execute bank services including take input command, get balance of an account, 
- * deposit in an account and withdraw from an account etc.
- * 
- * @author Konstantsin Liahk, Lau Kwai Fan
- * @since Java8
+ * represents bank services for a bank account.
+ * @author LauKwaiFanHK
+ * @since 1.0
  * @version 1
  *
  */
 
 public class BankAccountService {
 
+	/** Represents the string of command to get balance of a bank account.
+	*/
     private static final String GETBALANCE_COMMAND = "getBalance";
+    /** Represents the string of command to deposit money to a bank account.
+	*/
     private static final String DEPOSIT_COMMAND = "deposit";
+    /** Represents the string of command to withdraw money from a bank account.
+	*/
     private static final String WITHDRAW_COMMAND = "withdraw";
+    /** Represents the string of command to transfer money from a bank account
+     * to another bank account.
+	*/
     private static final String TRANSFER_COMMAND = "transfer";
 
+    /** Executes the command to get balance of a bank account.
+    *@return a double representing the balance of a bank account.
+    */
     public double executeGetBalance(String input, int numberOfArg, Integer bankAccountId, List<Integer> existedBankAccountIds, List<BaseAccount> list) {
         if (!isValidCommand(input, numberOfArg, GETBALANCE_COMMAND, 1)) {
             System.out.println("Invalid command: " + input);
@@ -35,6 +45,9 @@ public class BankAccountService {
         return account.getBalance();
     }
 
+    /** Executes the command to deposit money to a bank account.
+    *@return a double representing the balance of account after deposit.
+    */
     public double executeDeposit(String input, int numberOfArg, Integer bankAccountId, double amount, List<Integer> existedBankAccountIds, List<BaseAccount> list) {
         if (!isValidCommand(input, numberOfArg, DEPOSIT_COMMAND, 2)) {
             System.out.println("Invalid command: " + input);
@@ -56,6 +69,9 @@ public class BankAccountService {
         return bankAccount.getBalance();
     }
 
+    /** Executes the command to withdraw money from a bank account.
+    *@return a double representing the balance of the account after withdrawal. 
+    */
     public double executeWithdraw(String input, int numberOfArg,Integer bankAccountId, double amount, List<Integer> existedBankAccountIds, List<BaseAccount> list) {
         if (!isValidCommand(input, numberOfArg, WITHDRAW_COMMAND, 2)) {
             System.out.println("Invalid command: " + input);
@@ -84,6 +100,9 @@ public class BankAccountService {
         return bankAccount.getBalance();
     }
     
+    /** Executes the command to transfer money from a bank account to another bank account.
+     *@return a double representing the balance of the sender's account. 
+     */
     public double executeTransfer(String input, int numberOfArg, Integer senderAccountId, Integer receiverAccountId, double amount, List<Integer> existedSenderAccountIds, List<Integer> existedReceiverAccountIds, List<BaseAccount> list) {
         if (!isValidCommand(input, numberOfArg, TRANSFER_COMMAND, 5)) {
             System.out.println("Invalid command: " + input);
@@ -121,6 +140,9 @@ public class BankAccountService {
         return senderAccount.getBalance();
     }
 
+    /** Validate the input command.
+     *@return a boolean representing the state after validation. 
+     */
     private boolean isValidCommand(String input, int numberOfArg, String expectedCommand, int expectedArgs) {
         return expectedCommand.equals(input) && expectedArgs == numberOfArg;
     }
